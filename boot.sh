fda8cc89c51d4d179214386050e9a9526103d27b
joestar <joestar@lwork.com>
2019-11-03 00:04:59 +0800

    update

--- boot.sh
+++ boot.sh
@@ -12,9 +12,13 @@ ln -sf `pwd`/z.sh $HOME/.z.sh
 
 # neovim
 [ -d ~/.local/share/nvim/site/autoload/ ] || mkdir -p ~/.local/share/nvim/site/autoload/
-ln -sf `pwd`/plug.vim ~/.local/share/nvim/site/autoload/plug.vim
+ln -sf `pwd`/plug.vim ~/.local/share/nvim/site/autoload/
 [ -d ~/.config/nvim/ ] || mkdir ~/.config/nvim/
-ln -sf `pwd`/init.vim ~/.config/nvim/init.vim
+ln -sf `pwd`/init.vim ~/.config/nvim/
+
+# pip
+[ -d ~/.config/pip/ ] || mkdir ~/.config/pip/
+ln -sf `pwd`/pip.conf ~/.config/pip/
 
 # git
 ln -sf `pwd`/gitconfig.ini $HOME/.gitconfig
--- profile.sh
+++ profile.sh
@@ -13,7 +13,7 @@ alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 alias ..="cd .."
-alias vi="nvim"
+# alias vi="nvim"
 alias grep="grep --color=always"
 
 alias mongo="mongo mongodb://admin:leanwork2019@120.25.233.50:32006/admin"
@@ -21,7 +21,7 @@ alias mongo="mongo mongodb://admin:leanwork2019@120.25.233.50:32006/admin"
 PS1='\n\e[1;37m[\e[m\e[1;32m\u\e[m\e[1;33m@\e[m\e[1;35m\H\e[m:\e[4m$(pwd)\e[m\e[1;37m]\e[m\e[1;36m\e[m\n\$ '
 
 # PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-PATH="$PATH:/usr/local/go/bin"
+PATH="$HOME/.local/bin:/usr/local/go/bin:$PATH"
 
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 [ -f ~/.fzf.bash ] && source ~/.z.sh
